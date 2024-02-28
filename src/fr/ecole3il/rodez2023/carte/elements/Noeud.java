0566ccc049e23ec87ce46d42c455ace02d2f647f
KillianTurmorezoff <turmorek@3il.fr>
2024-02-28 11:01:54 +0100

    feat: ajout méthode ajouterVoisin

--- src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -1,5 +1,29 @@
 package fr.ecole3il.rodez2023.carte.elements;
 
-public class Noeud {
+import java.util.List;
 
+public class Noeud<E> {
+
+	private E valeur;
+	private List<Noeud<E>> voisin;
+	
+	public Noeud(E valeur) {
+		super();
+		this.valeur = valeur;
+		this.voisin = voisin;
+	}
+	
+	
+	public E getValeur() {
+		return valeur;
+	}
+	
+	public List<Noeud<E>> getVoisins(){
+		return voisin;
+	}
+	
+	public void ajouterVoisin(Noeud<E> newVoisin) {
+		voisin.add(newVoisin);
+	}
+	
 }
